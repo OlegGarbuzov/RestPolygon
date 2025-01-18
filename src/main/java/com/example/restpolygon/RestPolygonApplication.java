aad96139ecc2b0317427294afef76b7f89994696
ЩдупПфкигящм <mephisto_2005@mail.ru>
2025-01-18 12:25:06 +0300

    Initial commit

---
+++ src/main/java/com/example/restpolygon/RestPolygonApplication.java
@@ -0,0 +1,13 @@
+package com.example.restpolygon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPolygonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPolygonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restpolygon/RestPolygonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restpolygon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPolygonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
