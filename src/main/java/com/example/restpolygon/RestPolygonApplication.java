6c8a34f4bd80d2dfadb4cde436a3309f3d60cf2a
ЩдупПфкигящм <mephisto_2005@mail.ru>
2025-02-05 00:53:50 +0300

    Мелкие правки

--- src/main/java/com/example/restpolygon/RestPolygonApplication.java
+++ src/main/java/com/example/restpolygon/RestPolygonApplication.java
@@ -3,8 +3,6 @@ package com.example.restpolygon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,13 +10,6 @@ public class RestPolygonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestPolygonApplication.class, args);
-	}
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-		PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		placeholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
-		return placeholderConfigurer;
 	}
 
 }
